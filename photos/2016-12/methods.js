4d645f3081bcebec7bf89d5047fc5245119f73e6
Kurt Wegner <kurt.wegner@gmail.com>
2016-12-28 13:45:21 +0700

    Upload from Bogor

---
+++ photos/2016-12/methods.js
@@ -0,0 +1,43 @@
+
+/*******************************************************************************
+* methods of the class Pictures
+*******************************************************************************/
+function listOfImages()
+{
+    return [
+    "IMG_0115.JPG",
+    "IMG_1084.jpg",
+    "IMG_1099.JPG",
+    "IMG_1131.JPG",
+    "IMG_3003.JPG",
+    "IMG_3008.JPG",
+    "IMG_3011.JPG",
+    "IMG_3014.JPG",
+    "IMG_3015.JPG",
+    "IMG_3016.JPG",
+    "IMG_3017.JPG",
+    "IMG_3018.JPG",
+    "IMG_3019.JPG",
+    "IMG_3021.JPG",
+    "IMG_3023.JPG",
+    "IMG_3024.JPG",
+    "IMG_3026.JPG",
+    "IMG_3027.JPG",
+    "IMG_3029.JPG",
+    "IMG_3032.JPG",
+    "IMG_3035.JPG",
+    "IMG_3041.JPG",
+    "IMG_3042.JPG",
+    "IMG_3045.JPG",
+    "IMG_3048.JPG",
+    "IMG_3049.JPG",
+    "IMG_3052.JPG",
+    "IMG_3053.JPG",
+    "IMG_3054.JPG",
+    "IMG_3057.JPG",
+    "Levi.jpg",
+    "Rayna2.jpg",
+    ];
+}
+
+
