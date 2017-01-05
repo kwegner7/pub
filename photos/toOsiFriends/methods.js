364d5421219bdd1c651d29c08491f543cc748aa1
Kurt Wegner <kurt.wegner@gmail.com>
2017-01-05 13:13:33 +0800

    Share Indonesia Pictures With OSI Friends

---
+++ photos/toOsiFriends/methods.js
@@ -0,0 +1,51 @@
+
+/*******************************************************************************
+* methods of the class Pictures
+*******************************************************************************/
+function listOfImages()
+{
+    return [
+    "IMG_1046.jpg",
+    "IMG_1084.jpg",
+    "IMG_1099.JPG",
+    "IMG_1451.JPG",
+    "IMG_1868.JPG",
+    "IMG_2175.jpg",
+    "IMG_2177.jpg",
+    "IMG_2333.jpg",
+    "IMG_3008.JPG",
+    "IMG_3014.JPG",
+    "IMG_3015.JPG",
+    "IMG_3017.JPG",
+    "IMG_3018.JPG",
+    "IMG_3021.JPG",
+    "IMG_3023.JPG",
+    "IMG_3024.JPG",
+    "IMG_3026.JPG",
+    "IMG_3029.JPG",
+    "IMG_3032.JPG",
+    "IMG_3035.JPG",
+    "IMG_3041.JPG",
+    "IMG_3042.JPG",
+    "IMG_3045.JPG",
+    "IMG_3048.JPG",
+    "IMG_3049.JPG",
+    "IMG_3051.JPG",
+    "IMG_3053.JPG",
+    "IMG_3054.JPG",
+    "IMG_3057.JPG",
+    "IMG_3059.JPG",
+    "IMG_3063.JPG",
+    "IMG_3066.JPG",
+    "IMG_3071.JPG",
+    "IMG_3079.JPG",
+    "IMG_3081.JPG",
+    "IMG_3084.JPG",
+    "IMG_3087.JPG",
+    "IMG_3090.JPG",
+    "Levi.jpg",
+    "Rayna2.jpg",
+    ];
+}
+
+
