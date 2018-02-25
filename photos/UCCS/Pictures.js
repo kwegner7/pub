d4f73b2c8c0ef53dea90b8bfec8bbec2653678f8
Kurt Wegner <kurt.wegner@gmail.com>
2018-02-24 18:15:26 -0700

    Update Photos of UCCS

---
+++ photos/UCCS/Pictures.js
@@ -0,0 +1,48 @@
+
+/*******************************************************************************
+* virtual methods of the class Pictures
+*******************************************************************************/
+function listOfImages()
+{
+    return [
+ 
+    "IMG_3722.jpg",
+    "IMG_3727.jpg",
+    "IMG_3732.jpg",
+    "IMG_3739.jpg",
+    "IMG_3769.jpg",
+    "IMG_3778.jpg",
+    "IMG_3780.jpg",
+    "IMG_3783.jpg",
+    "IMG_3784.jpg",
+    "IMG_3790.jpg",
+    "IMG_3792.jpg",
+    "IMG_3794.jpg",
+    "IMG_3795.jpg",
+    "IMG_3797.jpg",
+    "IMG_3798.jpg",
+    "IMG_3802.jpg",
+    "IMG_3804 (1).jpg",
+    "IMG_3808.jpg",
+    "IMG_3810.jpg",
+    "IMG_3812 (1).jpg",
+    "IMG_3816.jpg",
+    "IMG_3817.jpg",
+    "IMG_3819.jpg",
+    "IMG_3822.jpg",
+    "IMG_3823 (1).jpg",
+    "IMG_3826.jpg",
+    "IMG_3827.jpg",
+    "IMG_3828.jpg",
+    "IMG_3830 (1).jpg",
+    "IMG_3831.jpg",
+    "IMG_3832.jpg",
+    "IMG_3833.jpg",
+    "IMG_3834.jpg",
+    "IMG_3836.jpg",
+    "IMG_3839.jpg",
+    "IMG_3842.jpg",
+    "IMG_3850.jpg",
+    ];
+}
+ 
